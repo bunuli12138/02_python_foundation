f7a12685980bf077dc690a2496cc5897b46c7dc8
bunuli12138 <wuxinbei.work@gmail.com>
2019-02-18 17:20:05 +0800

    use '__slot__' to restrict function parameters difinition

--- 08objective_higher_coding/01para_slot.py
+++ 08objective_higher_coding/01para_slot.py
@@ -6,7 +6,7 @@
 @File       :01para_slot.py
 @Time       :2019/2/18 16:13
 @Version    :
-@Purpose    :
+@Purpose    :__slots__定义的限制仅对当前类实例起作用，对继承的子类是不起作用的
 """
 import functools, time
 
@@ -22,6 +22,30 @@ def run_time(func):
 
     return wrapper
 
+class Student(object):
+    __slots__ = ('name', 'score', 'age', 'gender', 'sl') # 可以限制绑定方法
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+class H_stu(Student):
+    pass
+
+def sl():
+    time.sleep(0.012)
+    print('sl is running')
+
+def p():
+    print('pppppp')
 
 if __name__ == '__main__':
+    wy = Student('wy', 75)
+    wy.sl = sl
+    wy.sl()
+    # wy.p = p # 无法绑定
+    # wy.p()
+
+    h = H_stu('hsn', 99)
+    h.p = p  # 继承类实例可以绑定
+    h.p()
     pass
