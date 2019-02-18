62fc9fd3b00d84e05a4fe7efcc6bb6030128f71f
bunuli12138 <wuxinbei.work@gmail.com>
2019-02-18 16:10:10 +0800

    objective relevant restriction, inheritance, object message getting and parameters

--- 07objective_oriented_coding/01class_instance.py
+++ 07objective_oriented_coding/01class_instance.py
@@ -23,5 +23,29 @@ def run_time(func):
     return wrapper
 
 
+
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+
 if __name__ == '__main__':
-    pass
+    lisa = Student('Lisa', 99)
+    bart = Student('Bart', 59)
+    print(lisa.name, lisa.get_grade())
+    print(bart.name, bart.get_grade())
+
+    # python允许实例绑定不同的类定义没有的属性
+    lisa.age = 18
+    print(lisa.age)
+    # print(bart.age) # bart不存在age属性
--- 07objective_oriented_coding/02acess_restriction.py
+++ 07objective_oriented_coding/02acess_restriction.py
@@ -19,9 +19,63 @@ def run_time(func):
         result = func(*args, **kw)
         print('%s execute %.6f s' % (func.__name__, time.time() - t))
         return result
-
     return wrapper
 
 
+# 运行提醒
+def run_mes(cla):
+    @functools.wraps(cla)
+    def wrapper(*args, **kw):
+        result = cla(*args, **kw)
+        print('%s is running...' % cla.__name__)
+        return result
+    return wrapper
+
+
+@run_mes # 装饰器可以用于类
+class Student(object):
+    def __init__(self, name, score, gender):
+        self.__name = name
+        self.__score = score
+        self.gender = gender
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+
+    def get_gender(self):
+        return self.gender
+
+    def set_gender(self, gender):
+        self.gender = gender
+
+
 if __name__ == '__main__':
-    pass
+    wy = Student('wy', 75, 'male')
+    print(wy.get_name())
+
+    print(hasattr(wy, 'gender'))
+    print(getattr(wy, 'name', 404))
+    print(getattr(wy, 'set_gender'))
+    # wy._Student__name = 'hsn' # 私有变量依然可能被修改
+    # print(wy.get_name())
+
+    # 测试gender的两个方法
+    bart = Student('Bart', 99, 'male')
+    if bart.get_gender() != 'male':
+        print('测试失败!')
+    else:
+        bart.set_gender('female')
+        if bart.get_gender() != 'female':
+            print('测试失败!')
+        else:
+            print('测试成功!')
+
