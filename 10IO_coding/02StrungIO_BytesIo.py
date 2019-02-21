fdc56811c6a8a46af4e20c0ab9605194d77b0dd0
bunuli12138 <wuxinbei.work@gmail.com>
2019-02-21 14:13:42 +0800

    computer memory read and write method

--- 10IO_coding/02StrungIO_BytesIo.py
+++ 10IO_coding/02StrungIO_BytesIo.py
@@ -8,20 +8,45 @@
 @Version    :
 @Purpose    :
 """
-import functools, time
+from io import StringIO, BytesIO
+
+"""
+创建一个StringIO，然后，像文件一样写入
+"""
+sio = StringIO()
+
+sio.write('hello')
+sio.write(' ')
+sio.write('world')
+
+print(sio.getvalue())
+
+"""
+读取StringIO，可以用一个str初始化StringIO，然后，像读文件一样读取
+"""
+sio2 = StringIO('Hello!\nHi!\nGoodbye!')
+
+while True:
+    s = sio2.readline()
+    if s == '':
+        break
+    print(s.strip())
 
 
-# 计算函数运行时间的装饰器
-def run_time(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kw):
-        t = time.time()
-        result = func(*args, **kw)
-        print('%s execute %.6f s' % (func.__name__, time.time() - t))
-        return result
+# 二进制数据
+"""
+创建一个BytesIO，然后写入一些bytes
+"""
+bio = BytesIO()
 
-    return wrapper
+bio.write('中文'.encode('utf-8'))
 
+print(bio.getvalue())
 
+"""
+用一个bytes初始化BytesIO，然后，像读文件一样读取
+"""
+bio2 = BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+bio2.read()
 if __name__ == '__main__':
     pass
